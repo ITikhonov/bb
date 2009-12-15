9eadf57b65ac37dd8c48476425ea9857d4784f20
kef <kef@ivan-laptop.(none)>
2009-12-15 17:26:26 +0500

    changing interpreter to address-based

--- i.py
+++ i.py
@@ -1,14 +1,18 @@
 from operator import div
 from sys import stdin,stderr
 
-c=stdin
+prog=[]
+
+for l in stdin:
+	prog.append(l.split())
+
 r=[[]]
 s=r[0]
 v={'pow':pow}
+ip=0
 
-for l in c:
-	l=l.split()
-
+while ip<len(prog):
+	l=prog[ip]
 	if l[0]=='ident': s.append(l[1])
 	elif l[0]=='num': s.append(float(l[1]))
 	elif l[0]=='op+': s.append(s.pop()+s.pop())
@@ -28,6 +32,7 @@ for l in c:
 	elif l[0]=='com': pass
 	else: raise Exception(str(l))
 	stderr.write(str((l,r,v))+'\n')
+	ip+=1
 
 assert s==[],s
 assert v=={'a': 13.5, 'c': 0.66666666666666663, 'b': 10.0, 'd': 1.0, 'e':14.666666666666666,'f0':531441.0,'f':31429.891510774083,'pow':pow,'g':3.0,'h':7.0},v
